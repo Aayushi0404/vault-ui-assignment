6ba0d8e4da7184608fcca2f367423d70f5725075
Aayushi0404 <vaayushi314@gmail.com>
2025-07-20 21:41:03 +0530

    Add base HTML layout and header styles

--- script.js
+++
@@ -1,5 +0,0 @@
-document.querySelector(".btn").addEventListener("click", function(e) {
-    e.preventDefault();
-    document.querySelector("#features").scrollIntoView({ behavior: "smooth" });
-  });
-  
